5ef5514dcb989831b033af5b28f1d8b40d52969e
Sebastian Knapp <giftnuss@netscape.net>
2010-03-11 01:13:36 +0100

    simple inline defined accessors

--- ex/sample1/include/erm.h
+++ ex/sample1/include/erm.h
@@ -7,13 +7,14 @@
 
 #include <dbix/types.h>
 #include <dbix/table.h>
+#include <dbix/record.h>
 
 class erm {
  public:
   erm();
-  dbix::table& entity();
-  dbix::table& attribute();
-  dbix::table& relationship_role();
+  dbix::table& entity() { return entities; };
+  dbix::table& attribute() { return attributes; };
+  dbix::table& relationship_role() { return relationship_roles; };
  private:
   dbix::type_container tc;
   dbix::table entities;

63b84cee8a97529e7081fb726cef5664325b5681
Sebastian Knapp <giftnuss@netscape.net>
2010-03-11 01:16:21 +0100

    with implemented as template function

--- include/dbix/record.h
+++ include/dbix/record.h
@@ -4,10 +4,11 @@
 #define _DBIX_RECORD_H_
 
 #include <string>
+#include <vector>
+#include <dbix/table.h>
+#include <dbix/value.h>
 
 namespace dbix {
-
-  class table;
 
 class record
 {
@@ -15,12 +16,21 @@ class record
   friend class dbix::table;
   record(const dbix::table&);
  public:
-  record& with(const std::string&,int);
-  record& with(const std::string&,double);
-  record& with(const std::string&,std::string);
+  template <typename T>
+    record& with(const std::string&, T);
  private:
   const dbix::table* tableptr;
+  std::vector<dbix::value> values;
 };
+
+
+template <typename T> 
+record&
+  record::with(const std::string& key,T val)
+{
+  values.at(tableptr->column_pos(key)).is(val);
+  return *this;
+}
 
 }
 

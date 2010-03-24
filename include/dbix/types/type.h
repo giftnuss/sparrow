82085334dcfee7928b1cddd890ac743b7bc5c44a
Sebastian Knapp <giftnuss@netscape.net>
2010-03-24 20:44:19 +0100

    cleanup

--- include/dbix/types/type.h
+++ include/dbix/types/type.h
@@ -2,23 +2,22 @@
 #ifndef _DBIX_TYPES_TYPE_H_
 #define _DBIX_TYPES_TYPE_H_
 
-#include <stdexcept>
 #include <dbix/value.h>
 
 namespace dbix {
   namespace types {
 
-class type
-{
- public:
-  type();
-  virtual ~type() = 0;
-  int id() { return identity; }
- public:
-  virtual void valid_value(const std::string&, dbix::value* = NULL) = 0;
- protected:
-  int identity;
-};
+    class type
+    {
+     public:
+      type();
+      virtual ~type() = 0;
+      int id() { return identity; }
+     public:
+      virtual void valid_value(const std::string&, dbix::value* = NULL) = 0;
+     protected:
+      int identity;
+    };
 
   }
 }

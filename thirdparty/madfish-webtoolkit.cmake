0e0d55daf5c9a8445d6752c4828acf75840ad37a
personal account <ftr@.(none)>
2010-05-20 09:36:40 +0200

    building first thirdpartylibry further improved

---
+++ thirdparty/madfish-webtoolkit.cmake
@@ -0,0 +1,43 @@
+
+ExternalProject_Add(
+  madfish-webtoolkit
+  PREFIX webtoolkit
+  SVN_REPOSITORY http://madfish-webtoolkit.googlecode.com/svn/trunk
+  INSTALL_COMMAND ""
+)
+
+SET( CORETOOLKIT_HEADERS
+    File.h
+    FileUtils.h
+    Logger.h
+    OSUtils.h
+    Socket.h
+    Stream.h
+    Thread.h
+    Util.h
+)
+
+SET( WEBTOOLKIT_HEADERS
+    Client.h
+    Http.h
+    HttpHelpers.h
+    Server.h
+    WebToolkit.h
+)
+
+copy_headers(madfish-webtoolkit CoreToolkit CORETOOLKIT_HEADERS)
+copy_headers(madfish-webtoolkit WebToolkit WEBTOOLKIT_HEADERS)
+
+set_static_library_name(LIB_CoreToolkit_FILE CoreToolkit)
+set_static_library_name(LIB_WebToolkit_FILE WebToolkit)
+
+copy_library(madfish-webtoolkit CoreToolkit ${LIB_CoreToolkit_FILE})
+copy_library(madfish-webtoolkit WebToolkit ${LIB_WebToolkit_FILE})
+
+ADD_DEPENDENCIES(
+    madfish-webtoolkit
+        THIRDPARTY_INCLUDES
+        SPARROW_LIBRARIES
+)
+
+

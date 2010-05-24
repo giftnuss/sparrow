1187934e401e4e0d1f8f53d3955e366611901300
personal account <ftr@x1-6-00-1f-d0-ad-45-a1.(none)>
2010-05-24 13:57:29 +0200

    second thirdparty lib

---
+++ thirdparty/codemer-libtpt.cmake
@@ -0,0 +1,33 @@
+
+ExternalProject_Add(
+  libtpt
+  PREFIX libtpt
+  SVN_REPOSITORY https://svn.github.com/giftnuss/libtpt
+  INSTALL_COMMAND ""
+)
+
+SET( LIBTPT_HEADERS
+    buffer.h
+    compat.h
+    iparse.h
+    object.h
+    parse.h
+    smartptr.h
+    symbols.h
+    token.h
+    tpt.h
+    tptexcept.h
+    tpttypes.h
+)
+
+copy_headers(libtpt inc/libtpt LIBTPT_HEADERS)
+
+set_static_library_name(LIB_libtpt_FILE tpt)
+
+copy_library(libtpt src/lib ${LIB_libtpt_FILE})
+
+ADD_DEPENDENCIES(
+    libtpt
+        THIRDPARTY_INCLUDES
+        SPARROW_LIBRARIES
+)
